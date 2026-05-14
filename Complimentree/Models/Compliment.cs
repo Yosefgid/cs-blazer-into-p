ccf994fa246abf30e713993a398ffb2162a4b7a3
Yosefgid <jossykasshun@yahoo.com>
2026-05-14 17:00:33 +0100

    feat: Add a method to Compliment taht returns a random joke

--- Complimentree/Models/Compliment.cs
+++ Complimentree/Models/Compliment.cs
@@ -4,5 +4,28 @@
     {
         public int Id { get; set; }
         public string Text { get; set; }
+        public static Random rnd = new();
+        private static List<string> complimentTexts = new()
+        {
+        "You may not be evergreen, but you are to me.",
+        "I bet squirrels consider you prime real estate.",
+        "You're so down to earth.",
+        "You're very grounded.",
+        "Your leaves are soothing.",
+        "You sway nicely in the wind.",
+        "You're blooming brilliant.",
+        "You're really good at standing still and doing nothing.",
+        "Let's get to the root of your beauty.",
+        "I will never leaf you.",
+        "You've really branched out lately.",
+        "You provide the perfect amount of shade on a hot day.",
+        "I'm really pine-ing for you."
+        };
+        public static Compliment GetCompliment(int id)
+        {
+            var complimentText = complimentTexts[rnd.Next(complimentTexts.Count)];
+            return new Compliment { Id = id, Text = complimentText };
+         
+        }
     }
 }
