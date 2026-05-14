99980922f5e75d494e8d04b2059db159320f7172
Yosefgid <jossykasshun@yahoo.com>
2026-05-14 16:32:59 +0100

    Add project files.

---
+++ Complimentree/Models/Tree.cs
@@ -0,0 +1,15 @@
+﻿namespace Complimentree.Models
+{
+    public class Tree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public (decimal Latitude, decimal Longitude ) Location { get; set; }
+        public string Size { get; set; }
+        public string Species { get; set; }
+        public List<Compliment> Compliments { get; set; } = new();
+
+
+
+    }
+}
